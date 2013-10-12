980480fb9c847a1879f073f4d7a91c307bb599c4
Jorge Pereyra <jorgepreeyra751@hotmail.com>
2013-10-12 12:48:23 -0300

    Issue #4 - Create a populator - Refactoring...

--- lib/populator.rb
+++ lib/populator.rb
@@ -4,7 +4,7 @@ module Populator
 
   def populate_projects!
     Project.destroy_all
-    [2, 3].sample.times do
+    [2,3].sample.times do
       p = Project.create(
         name: Faker::Company::name,
         description: Faker::Lorem.paragraphs.join("\n")
@@ -17,17 +17,17 @@ module Populator
     Task.destroy_all
     populate_projects! if Project.count(:id) == 0
     Project.all.each do |project|
-      [7, 9].sample.times do
+      [7,8,9].sample.times do
         t            = project.tasks.build
         t.name       = Faker::Company::name
         t.programmer = Faker::Name::name
-        t.priority   = Task::PRIORITY_DOMAIN[[0,1,2].sample]
+        t.priority   = Task::PRIORITY_DOMAIN.sample
         t.notes      = Faker::Lorem.paragraphs.join("\n")
         t.budget     = [100,200,300].sample
         t.start_date = nil
         t.due_date   = nil
-        t.status     = Task::STATUS_DOMAIN[[0,1,2].sample]
-        t.kind       = Task::KIND_DOMAIN[[0,1,2].sample]
+        t.status     = Task::STATUS_DOMAIN.sample
+        t.kind       = Task::KIND_DOMAIN.sample
         t.save!
       end
     end
